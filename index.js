a2671617c579dc9b665f484fc13d5a4fa4f2ea17
neoboi76 <dattimbol@mymail.mapua.edu.ph>
2023-11-10 17:02:53 +0800

    Add 'Ans' button and some comments

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-let calculation = localStorage.getItem('calculation');
+//Declarations
+
+let calculation = localStorage.getItem('calculation');//Saves current calculation in local storage
 document.querySelector('.js-field').innerHTML = calculation;
-let numbers = document.querySelectorAll('.js-numbers');
-let operators = document.querySelectorAll('.js-operators');
-let misc = document.querySelectorAll('.js-misc');
-let test = 0;
+let numbers = document.querySelectorAll('.js-numbers');//Numbers
+let operators = document.querySelectorAll('.js-operators');//Operators
+let misc = document.querySelectorAll('.js-misc');//Special symbols ('.', '=', 'Ans', etc.)
 let result = null;
 
+//Executables
 
+//Accounts for all numbers inputted
 numbers.forEach((num, index) => {
   num.addEventListener('click', (value, index) => {
 
@@ -44,36 +47,43 @@ misc.forEach((sym, index) => {
       }
     }
 
+    if (result !== null) {
+      result = null;
+      calculation = ' ';
+      updateCalculation(sym.innerText);
+    }
+
   })
 })
 
 function updateCalculation (strg) {
- 
+  
   if (strg == '=') {
 
     result = eval(calculation)
+    answer = result; 
     
     if (result !== undefined) {
       calculation = `= ${result}`;
     }
   }
 
+  else if (strg == 'Ans') {
+    calculation += answer;
+  } 
+
   else if (strg == 'Cleared') {
     calculation = ' ';
     result = null;
-    localStorage.removeItem(`calculation`);
+    localStorage.removeItem('calculation');
   }
-  
 
   else {
     calculation += strg;
   }
 
-  
 
-
-document.querySelector('.js-field').innerHTML = calculation;
-
-localStorage.setItem('calculation', calculation); 
+  document.querySelector('.js-field').innerHTML = calculation;
+  localStorage.setItem('calculation', calculation);
 
 }
